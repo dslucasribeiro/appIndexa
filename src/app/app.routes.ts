d0b3661c8efc851e5c2418527966f3c371cc6d17
dslucasribeiro <dslucasribeiro@gmail.com>
2024-02-28 21:34:56 -0300

    feat: adicionando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'lista-contatos', pathMatch: 'full'},
+    { path: 'lista-contatos', component: ListaContatosComponent },
+    { path: 'formulario', component: FormularioContatoComponent }
+];
